a7179143aa61fa5b1736319fbf4f139be6cb7ca8
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-06-17 12:11:31 +0100

    clean

--- tests/Interval/IntervalToStringTest.php
+++ tests/Interval/IntervalToStringTest.php
@@ -25,51 +25,96 @@ class IntervalToStringTest extends TestCase
         $this->assertEquals('PT0S', (string)$ci);
     }
 
+    /**
+     * Date section
+     */
     public function testYearInterval()
     {
         $ci = new ChronosInterval();
+        $ci1 = new ChronosInterval(1);
+        $ci2 = new ChronosInterval(0, 12, 0, 0);
+        $ci3 = new ChronosInterval(0, 0, 0, 365);
+        $ci4 = new ChronosInterval(0, 0, 52, 1);
+
         $this->assertEquals('P1Y', (string)$ci);
+        $this->assertEquals('P1Y', (string)$ci1);
+        $this->assertEquals('P1Y', (string)$ci2);
+        $this->assertEquals('P1Y', (string)$ci3);
+        $this->assertEquals('P1Y', (string)$ci4);
     }
 
     public function testMonthInterval()
     {
-        $ci = new ChronosInterval(0, 1);
-        $this->assertEquals('P1M', (string)$ci);
+        $ci1 = new ChronosInterval(0, 1);
+        $ci2 = new ChronosInterval(0, 0, 0, 30, 10);
+        $ci3 = new ChronosInterval(0, 0, 4, 2, 10);
+
+        $this->assertEquals('P1M', (string)$ci1);
+        $this->assertEquals('P1M', (string)$ci2);
+        $this->assertEquals('P1M', (string)$ci3);
     }
 
     public function testWeekInterval()
     {
-        $ci = new ChronosInterval(0, 0, 1);
-        $this->assertEquals('P7D', (string)$ci);
+        $ci1 = new ChronosInterval(0, 0, 1);
+        $ci2 = new ChronosInterval(0, 0, 0, 7);
+        $ci3 = new ChronosInterval(0, 0, 0, 0, 7*24);
+        $ci4 = new ChronosInterval(0, 0, 0, 0, 0, 7*24*60);
+
+        $this->assertEquals('P7D', (string)$ci1);
+        $this->assertEquals('P7D', (string)$ci2);
+        $this->assertEquals('P7D', (string)$ci3);
+        $this->assertEquals('P7D', (string)$ci4);
     }
 
     public function testDayInterval()
     {
-        $ci = new ChronosInterval(0, 0, 0, 1);
-        $this->assertEquals('P1D', (string)$ci);
+        $ci1 = new ChronosInterval(0, 0, 0, 1);
+        $ci2 = new ChronosInterval(0, 0, 0, 0, 24);
+        $ci3 = new ChronosInterval(0, 0, 0, 0, 0, 24*60);
+        $ci4 = new ChronosInterval(0, 0, 0, 0, 0, 0, 24*60*60);
+
+        $this->assertEquals('P1D', (string)$ci1);
+        $this->assertEquals('P1D', (string)$ci2);
+        $this->assertEquals('P1D', (string)$ci3);
+        $this->assertEquals('P1D', (string)$ci4);
     }
 
     public function testMixedDateInterval()
     {
-        $ci = new ChronosInterval(1, 2, 0, 3);
-        $this->assertEquals('P1Y2M3D', (string)$ci);
+        $ci1 = new ChronosInterval(1, 2, 0, 3);
+        $ci2 = new ChronosInterval(0, 14, 0, 3);
+        $this->assertEquals('P1Y2M3D', (string)$ci1);
+        $this->assertEquals('P1Y2M3D', (string)$ci2);
     }
 
+    /**
+     * Time section
+     */
     public function testHourInterval()
     {
-        $ci = new ChronosInterval(0, 0, 0, 0, 1);
-        $this->assertEquals('PT1H', (string)$ci);
+        $ci1 = new ChronosInterval(0, 0, 0, 0, 1);
+        $ci2 = new ChronosInterval(0, 0, 0, 0, 0, 60);
+        $ci3 = new ChronosInterval(0, 0, 0, 0, 0, 0, 3600);
+
+        $this->assertEquals('PT1H', (string)$ci1);
+        $this->assertEquals('PT1H', (string)$ci2);
+        $this->assertEquals('PT1H', (string)$ci3);
     }
 
     public function testMinuteInterval()
     {
-        $ci = new ChronosInterval(0, 0, 0, 0, 0, 1);
-        $this->assertEquals('PT1M', (string)$ci);
+        $ci1 = new ChronosInterval(0, 0, 0, 0, 0, 1);
+        $ci2 = new ChronosInterval(0, 0, 0, 0, 0, 0, 60);
+
+        $this->assertEquals('PT1M', (string)$ci1);
+        $this->assertEquals('PT1M', (string)$ci2);
     }
 
     public function testSecondInterval()
     {
         $ci = new ChronosInterval(0, 0, 0, 0, 0, 0, 1);
+
         $this->assertEquals('PT1S', (string)$ci);
     }
 
@@ -79,10 +124,16 @@ class IntervalToStringTest extends TestCase
         $this->assertEquals('PT1H2M3S', (string)$ci);
     }
 
+    /**
+     * Date and Time sections
+     */
     public function testMixedDateAndTimeInterval()
     {
-        $ci = new ChronosInterval(1, 2, 0, 3, 4, 5, 6);
-        $this->assertEquals('P1Y2M3DT4H5M6S', (string)$ci);
+        $ci1 = new ChronosInterval(0, 0, 0, 0, 48, 120);
+        $ci2 = new ChronosInterval(0, 24, 0, 0, 48, 120);
+
+        $this->assertEquals('P2DT2H', (string)$ci1);
+        $this->assertEquals('P2Y2DT2H', (string)$ci2);
     }
 
     public function testCreatingInstanceEquals()
