1066e97a403277071cc81a0376d8b2fb45e7d766
Corey Taylor <corey.taylor.fl@gmail.com>
2019-11-02 00:00:00 -0500

    Removed unnecessary copyright

--- tests/Benchmark/ConstructBench.php
+++ tests/Benchmark/ConstructBench.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @copyright     Copyright (c) Brian Nesbitt <brian@nesbot.com>
  * @link          http://cakephp.org CakePHP(tm) Project
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
