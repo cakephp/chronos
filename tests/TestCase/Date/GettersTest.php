64d3e479801af9e4ffb1403aeb77a8956e1439ab
breno <breno.jesus@ufms.br>
2023-09-19 18:22:30 -0400

    remove copyright docblock

--- tests/TestCase/Date/GettersTest.php
+++ tests/TestCase/Date/GettersTest.php
@@ -1,18 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/**
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @copyright     Copyright (c) Brian Nesbitt <brian@nesbot.com>
- * @link          https://cakephp.org CakePHP(tm) Project
- * @license       https://www.opensource.org/licenses/mit-license.php MIT License
- */
-
 namespace Cake\Chronos\Test\TestCase\Date;
 
 use Cake\Chronos\ChronosDate;
