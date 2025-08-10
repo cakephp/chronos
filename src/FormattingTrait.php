5493960958b085a9930b2ffd21d426f4a8f64527
mscherer <dereuromark@web.de>
2025-08-10 12:51:08 +0200

    Split out toQuarterRange() for cleaner return API.

--- src/FormattingTrait.php
+++ src/FormattingTrait.php
@@ -238,6 +238,7 @@ trait FormattingTrait
      *
      * @param bool $range Range.
      * @return array|int 1, 2, 3, or 4 quarter of year or array if $range true
+     * @deprecated 3.3.0 The $range parameter is deprecated. Use toQuarterRange() for quarter ranges.
      */
     public function toQuarter(bool $range = false): int|array
     {
@@ -246,7 +247,24 @@ trait FormattingTrait
             return $quarter;
         }
 
+        trigger_error(
+            'Using toQuarter() with `$range=true` is deprecated. Use `toQuarterRange()` instead.',
+            E_USER_DEPRECATED
+        );
+
+        return $this->toQuarterRange();
+    }
+
+    /**
+     * Returns the quarter range
+     *
+     * @return array<string> Array with start and end date of quarter in Y-m-d format
+     */
+    public function toQuarterRange(): array
+    {
+        $quarter = (int)ceil((int)$this->format('m') / 3);
         $year = $this->format('Y');
+
         switch ($quarter) {
             case 1:
                 return [$year . '-01-01', $year . '-03-31'];
